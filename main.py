66c5b008f4940de0d953f92038f39a9ef3d6f97e
Aquib Akhtar <makhtar@conncoll.edu>
2020-04-18 17:47:38 -0400

    Third Update

--- main.py
+++ main.py
@@ -3,9 +3,39 @@ def LoginPage():
                 1)Login
                 2)Sign Up
                 3)Quit
-                4)Edit
-                
     """)
+    userInput=input("Enter Option")
+    if userInput='1':
+        Login()
+    elif userInput='2':
+        NewAccount()
+    elif userInput='3':
+        closeGame()
+    else:
+        LoginPage()
+def closeGame():
+def NewAccount():
+    Print("Input your Credentials")
+    fname=input('Input your First Name')
+    email=input('Input your email')
+    passwordCheck=False
+    while not passwordCheck:
+        password=input('input password')
+        password2=input('input your password again')
+        if password==password2:
+            passwordCheck=True
+        else:
+            input('passwords were not the same')
+
+def Login():
+    username=input("Username:")
+    password=input("Password:")
+    usernameCheck=True #Check SQL for username and password, return with true or false
+    if usernameCheck:
+        mainPage()
+    else:
+        input("Login Failed, Press Enter to Try again")
+        Login()
 
 LoginPage()
 
