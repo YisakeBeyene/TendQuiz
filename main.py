35a8a328e29a25aebb3fb3ad4dd923e63f337b49
Isaac <tadeleyisake@gmail.com>
2020-04-18 17:41:03 -0400

    Mainpage options added

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 
 def mainPage():
     print("To play game press S:")
-    print("To see highscore press S")
-    print("TO change personal credentials press C")
-    print("To quit press Q")
+    print("To see highscore press H:")
+    print("TO change personal credentials press C:")
+    print("To quit press Q:")
+    value = input("Enter your input here:")
+
+    if value == 'S':
+        print("The function for questions page")
+    elif value == 'H':
+        print("The function for highscore page")
+    elif value == 'C':
+        print("The function for changeing credentials page")
+    elif value == 'Q':
+        print("The function for quiting")
